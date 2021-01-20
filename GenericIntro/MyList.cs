9ebb59b512a0997954ca6d88ea7e1d51bacd7b19
BGOuk <ummut22005@gmail.com>
2021-01-20 23:25:32 +0300

    BERKAY

---
+++ GenericIntro/MyList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+
+        public void Add(T item)
+        {
+
+        }
+    }
+}
---
+++ GenericIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("BERKAY"); // : )
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
